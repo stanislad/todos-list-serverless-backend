fa2c19c371fb5437bc2f0b749c3704a4fbf3922f
Stanislav Synenko <Stanislav.Synenko@bionic.co.uk>
2025-01-22 11:18:01 +0000

    Revert "build tidy"
    This reverts commit 5c8b56d360edaf4d86d40b906440178c876389ee.

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST='{"../node_modules/@tanstack/query-devtools/build/index.js -> ./DevtoolsComponent/HO4MOOFI.js":{"id":null,"files":[]},"../node_modules/@tanstack/query-devtools/build/index.js -> ./DevtoolsPanelComponent/HUY7CZI3.js":{"id":null,"files":[]}}';
